0eb9d4fd7fe505f19473c685f3b5c7c08aba5ae0
eliseugabrielsilva <49133854+eliseugabrielsilva@users.noreply.github.com>
2025-05-29 19:54:51 -0300

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,11 +1,1 @@
-Cypress.Commands.add('apiRequest', (method, endpoint, body = null) => {
-    return cy.request({
-        method,
-        url: endpoint,
-        headers: {
-            'Content-Type': 'application/json',
-            'Ocp-Apim-Subscription-Key' : `${Cypress.env('authToken')}`
-        },
-        body
-    })
-})
+
