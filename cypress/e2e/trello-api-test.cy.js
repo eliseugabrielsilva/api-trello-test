156718981521a6e8a43504ce132bbcb747857ffa
eliseugabrielsilva <49133854+eliseugabrielsilva@users.noreply.github.com>
2025-05-29 19:47:27 -0300

    Update trello-api-test.cy.js

--- cypress/e2e/trello-api-test.cy.js
+++ cypress/e2e/trello-api-test.cy.js
@@ -1,59 +1,52 @@
-describe('Trello teste API', () => {
 
-    beforeEach(() => {
-      Cypress.config('requestTimeout', 10000);
+
+describe('Automação Trello API', () => {
+  let boardId;
+  let cardId = '';
+  let listId = '';
+
+  it('Cadastrar um board', () => {
+    cy.request({
+      method: 'POST',
+      url: `/boards/?name=BoardTeste&key=${key}&token=${token}`
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      boardId = response.body.id;
     });
-  
-    it('Cadastrar um board', () => {
+  });
+
+  it('Cadastrar um card', () => {
+    cy.request({
+      method: 'GET',
+      url: `/boards/${boardId}/lists?key=${key}&token=${token}`
+    }).then((res) => {
+      listId = res.body[0].id;
+
       cy.request({
-        url: 'https://apibr-dev.edenred.com/bureau/antifraude/api/analisecompliance/analisar',
         method: 'POST',
-        body: {
-          nome: 'ELISEU GABRIEL',
-          documento: '35821014875'
-        }
-        
+        url: `/cards?name=CardTeste&idList=${listId}&key=${key}&token=${token}`
       }).then((response) => {
         expect(response.status).to.eq(200);
-        cy.log(`POST simulado retornou ID: ${response.body.id}`);
-        expect(response.body.tipoAcaoRating).to.eq("LiberarAutomaticamente");
-        expect(response.body.rating.tipoRiscoRating).to.eq("Baixo");
+        cardId = response.body.id;
       });
     });
-         
-    });
+  });
 
+  it('Excluir um card', () => {
+    cy.request({
+      method: 'DELETE',
+      url: `/cards/${cardId}?key=${key}&token=${token}`
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+    });
+  });
 
-    it('Cadastrar um card', () => {
-      cy.request({
-        url: 'https://jsonplaceholder.typicode.com/posts',
-        method: 'GET'
-      }).then((response) => {
-        expect(response.status).to.eq(200);
-        expect(response.body).to.be.an('array').and.not.to.be.empty;
-        expect(response.body[0].title).to.eq("sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
-      });
+  it('Excluir um board', () => {
+    cy.request({
+      method: 'DELETE',
+      url: `/boards/${boardId}?key=${key}&token=${token}`
+    }).then((response) => {
+      expect(response.status).to.eq(200);
     });
-
-    it('Excluir um card', () => {
-      cy.request({
-        url: 'https://jsonplaceholder.typicode.com/posts',
-        method: 'GET'
-      }).then((response) => {
-        expect(response.status).to.eq(200);
-        expect(response.body).to.be.an('array').and.not.to.be.empty;
-        expect(response.body[0].title).to.eq("sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
-      });
-    });
-    
-    it('Excluir um board', () => {
-      cy.request({
-        url: 'https://jsonplaceholder.typicode.com/posts',
-        method: 'GET'
-      }).then((response) => {
-        expect(response.status).to.eq(200);
-        expect(response.body).to.be.an('array').and.not.to.be.empty;
-        expect(response.body[0].title).to.eq("sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
-      });
-    });
-    
+  });
+});
